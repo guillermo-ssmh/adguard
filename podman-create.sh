5cef7a3b05d9b03dce615c96af334b944605bd3d
guillermo-ssmh <guillermo.ssmh@gmail.com>
2025-06-14 14:49:22 +0200

    Update podman-create.sh

--- podman-create.sh
+++ podman-create.sh
@@ -2,7 +2,7 @@ docker run --name adguardhome \
 --restart unless-stopped    \
 -v /opt/adguard/work:/opt/adguardhome/work    \
 -v /opt/adguard/conf:/opt/adguardhome/conf    \
--d adguard/adguardhome \
+-d \
 -p 53:53/tcp -p 53:53/udp    \
 -p 67:67/udp -p 68:68/udp    \
 -p 80:80/tcp \
@@ -11,4 +11,5 @@ docker run --name adguardhome \
 -p 853:853/tcp      -p 853:853/udp \
 -p 5443:5443/tcp    -p 5443:5443/udp  \
 -p 8853:8853/udp \
---network=host --cap-add=NET_ADMIN --cap-add=NET_RAW
+--network=host --cap-add=NET_ADMIN --cap-add=NET_RAW \
+adguard/adguardhome
